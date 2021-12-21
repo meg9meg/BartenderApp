94a24547ad66546711fa47a4d803261ab166c8d6
meg9meg <magda110807@gmail.com>
2021-12-21 21:55:13 +0100

    add drink details, preparing final design - first part

--- script.js
+++ script.js
@@ -3,10 +3,17 @@ const alphabethLetter = document.querySelectorAll('.dictionary__letter');
 const drinksPanel = document.querySelector('.drinks');
 const noDrinks = document.querySelector('.drinks__no-drinks');
 
+const fullScreen = document.querySelector('.fullscreen-container');
+
+const drinkDetails = document.querySelector('.details');
+const drinkName = document.querySelector('.details__name');
+const drinkIngredients = document.querySelector('.details__ingredients');
+const drinkPrep = document.querySelector('.details__prep');
+const closeBtn = document.querySelector('.details__close-btn');
 
 const API_LINK = "https://thecocktaildb.com/api/json/v1/1/search.php?f=";
-
 const API_BY_NAME = "https://thecocktaildb.com/api/json/v1/1/search.php?s=";
+const API_BY_ID = "https://thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 
 const findDrinks = (letter) => {
     drinksPanel.textContent = "";
@@ -18,6 +25,8 @@ const findDrinks = (letter) => {
         drinks.forEach(drink => {
             const item = document.createElement('div');
             item.className = "drinks__drink";
+            item.setAttribute("onclick", `showDrinkDetails(${drink.idDrink})`);
+            item.id = drink.idDrink;
 
             item.innerHTML = 
             `
@@ -25,6 +34,12 @@ const findDrinks = (letter) => {
                 <img class="drinks__drink__image" src="${drink.strDrinkThumb}"/>`
             drinksPanel.appendChild(item);
         })
+    }).catch(() => {
+        drinksPanel.innerHTML = `
+        <div class="drinks__no-drinks">
+            <i class="fas fa-glass-martini-alt drinks__no-drinks__icon"></i>
+            <h2 class="drinks__no-drinks__zero">We don't find drink you requested :(</h2> 
+        </div>`
     })
 }
 
@@ -39,6 +54,8 @@ const searchByName = () => {
         drinks.forEach(drink => {
             const item = document.createElement('div');
             item.className = "drinks__drink";
+            item.setAttribute("onclick", `showDrinkDetails(${drink.idDrink})`);
+            item.id = drink.idDrink;
 
             item.innerHTML = 
             `
@@ -57,6 +74,45 @@ const searchByName = () => {
     
 }
 
+const showDrinkDetails = id => {
+    const drink = document.getElementById(id);
+    drinkDetails.classList.remove("hidden");
+    fullScreen.classList.remove("hidden");
+
+    const URL = API_BY_ID + id;
+    console.log(URL);
+
+    axios.get(URL).then(res => {
+        const drink = res.data.drinks[0];
+        drinkName.textContent = `${drink.strDrink}`;
+        drinkPrep.textContent = `${drink.strInstructions}`;
+
+        //ingredients
+        const ingredients = [[drink.strIngredient1, drink.strMeasure1],[drink.strIngredient2, drink.strMeasure2],[drink.strIngredient3, drink.strMeasure3],[drink.strIngredient4, drink.strMeasure4],
+                    [drink.strIngredient5, drink.strMeasure5],[drink.strIngredient6, drink.strMeasure6],[drink.strIngredient7, drink.strMeasure7],[drink.strIngredient8, drink.strMeasure8],
+                    [drink.strIngredient9, drink.strMeasure9],[drink.strIngredient10, drink.strMeasure10],[drink.strIngredient11, drink.strMeasure11],[drink.strIngredient12, drink.strMeasure12],
+                    [drink.strIngredient13, drink.strMeasure13],[drink.strIngredient14, drink.strMeasure14],[drink.strIngredient15, drink.strMeasure15]];
+
+        for(let i = 0; i < ingredients.length; i++){
+            let ingredient = ingredients[i][0];
+            let measure = ingredients[i][1];
+            if(ingredient != null){
+                drinkIngredients.innerHTML += `<p class="details__ingredients_ingredient">${ingredient} ${measure==null ? "" : measure}</p>`;
+            }
+            else return;
+        }
+        })
+}
+
+const closeDetails = () => {
+    drinkDetails.classList.add("hidden");
+    fullScreen.classList.add("hidden");
+    drinkName.textContent = "";
+    drinkIngredients.textContent = "";
+    drinkPrep.textContent = "";
+}
+
+
 alphabethLetter.forEach(item => {
     item.addEventListener('click', () => {
         findDrinks(item.textContent);
@@ -64,6 +120,7 @@ alphabethLetter.forEach(item => {
 });
 
 inputDrinkName.addEventListener("input", searchByName);
+closeBtn.addEventListener("click", closeDetails);
 
 window.onload = function(){
     inputDrinkName.value = "";
